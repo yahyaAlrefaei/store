daf887abe959d780d5ed7b9a9f9f058ea5450ac7
Yahya Alrefaei <yahyaalrefaei@geexar.dev>
2024-05-27 09:24:37 +0300

    edit form

--- src/pages/auth/Signup/index.tsx
+++ src/pages/auth/Signup/index.tsx
@@ -1,18 +1,18 @@
 import useSignup from "./useSignup";
 
 const Signup = () => {
-    const {submit} = useSignup()
+    const {submit , formRef} = useSignup()
     return (<>
     <main className="flex justify-center items-center h-svh">
        <div className="bg-gray-200 p-8 w-[100%] max-w-[500px]">
         <h2>Signup</h2>
-        <form className="grid">
+        <form className="grid" onSubmit={submit}>
             <input className="border border-gray-200 h-12 w-[100%] mb-3 p-2" type="text" name="first_name" />
             <input className="border border-gray-200 h-12 w-[100%] mb-3 p-2" type="text" name="last_name" />
             <input className="border border-gray-200 h-12 w-[100%] mb-3 p-2" type="email" name="email" />
             <input className="border border-gray-200 h-12 w-[100%] mb-3 p-2" type="password" name="pasword" />
             <input className="border border-gray-200 h-12 w-[100%] mb-3 p-2" type="password" name="confirm_password" />
-            <button onClick={submit}>submit</button>
+            <button type="submit">submit</button>
         </form>
        </div>
     </main>
--- src/pages/auth/Signup/useSignup.ts
+++ src/pages/auth/Signup/useSignup.ts
@@ -1,11 +1,34 @@
-const useSignup = () => {
+import { ComponentProps, useRef } from "react";
+import http from "../../../config/axios";
 
-    const submit = () => {
+const useSignup = () => {
+    const formRef = useRef<ComponentProps<"form">>()
+
+    const defalutValues = {
+        first_name:"",
+        last_name:"",
+        email:"",
+        password:"",
+        confirm_password:""
+    }
+
+    const submit = (e:any) => {
+        e.preventDefault()
+        console.log(e);
+        
+        // console.log(formRef.current);
+        
         console.log("print form yahya");
+        try {
+            // const res = http.post()
+        } catch (error) {
+            
+        }
         
     }
     return {
-        submit
+        submit,
+        formRef
     };
 }
  
