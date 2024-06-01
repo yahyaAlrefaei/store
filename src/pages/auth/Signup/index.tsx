c429ca75d418fdb568c595e2b8ce9f3d2088fd97
Yahya Alrefaei <yahya.alrefaei1@gmail.com>
2024-06-01 22:23:22 +0300

    react hook form

--- src/pages/auth/Signup/index.tsx
+++ src/pages/auth/Signup/index.tsx
@@ -1,17 +1,17 @@
 import useSignup from "./useSignup";
 
 const Signup = () => {
-    const {submit , formRef} = useSignup()
+    const {onSubmit , formRef, handleSubmit, register} = useSignup()
     return (<>
     <main className="flex justify-center items-center h-svh">
        <div className="bg-gray-200 p-8 w-[100%] max-w-[500px]">
         <h2>Signup</h2>
-        <form className="grid" onSubmit={submit}>
-            <input className="border border-gray-200 h-12 w-[100%] mb-3 p-2" type="text" name="first_name" />
-            <input className="border border-gray-200 h-12 w-[100%] mb-3 p-2" type="text" name="last_name" />
-            <input className="border border-gray-200 h-12 w-[100%] mb-3 p-2" type="email" name="email" />
-            <input className="border border-gray-200 h-12 w-[100%] mb-3 p-2" type="password" name="pasword" />
-            <input className="border border-gray-200 h-12 w-[100%] mb-3 p-2" type="password" name="confirm_password" />
+        <form className="grid" onSubmit={handleSubmit(onSubmit)}>
+            <input className="border border-gray-200 h-12 w-[100%] mb-3 p-2" type="text" {...register("first_name")} />
+            <input className="border border-gray-200 h-12 w-[100%] mb-3 p-2" type="text" {...register("last_name")} />
+            <input className="border border-gray-200 h-12 w-[100%] mb-3 p-2" type="email" {...register("email")} />
+            <input className="border border-gray-200 h-12 w-[100%] mb-3 p-2" type="password" {...register("password")} />
+            <input className="border border-gray-200 h-12 w-[100%] mb-3 p-2" type="password" {...register("firstconfirm_password_name")} />
             <button type="submit">submit</button>
         </form>
        </div>
--- src/pages/auth/Signup/useSignup.ts
+++ src/pages/auth/Signup/useSignup.ts
@@ -1,8 +1,10 @@
-import { ComponentProps, useRef } from "react";
+import { ComponentProps, FormEvent, useRef } from "react";
 import http from "../../../config/axios";
+import { SubmitHandler, useForm } from "react-hook-form"
 
 const useSignup = () => {
     const formRef = useRef<ComponentProps<"form">>()
+    const { register, handleSubmit } = useForm()
 
     const defalutValues = {
         first_name:"",
@@ -12,23 +14,16 @@ const useSignup = () => {
         confirm_password:""
     }
 
-    const submit = (e:any) => {
-        e.preventDefault()
-        console.log(e);
-        
-        // console.log(formRef.current);
-        
-        console.log("print form yahya");
-        try {
-            // const res = http.post()
-        } catch (error) {
-            
-        }
+    const onSubmit = (data: any) => {
+     
+        console.log(data);
         
     }
     return {
-        submit,
-        formRef
+        onSubmit,
+        formRef,
+        register,
+        handleSubmit
     };
 }
  
